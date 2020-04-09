0aa6508e3972d271b14941606b6284fd510685af
z004mr2 <mike.juarezsweeney@target.com>
2020-04-09 16:07:54 -0500

    Added PUT, DELETE functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import MainPage from "./components/MainPage";
-import FetchNames from "./components/FetchNames";
 function App() {
   return (
     <>
       <MainPage />
-      <FetchNames />
     </>
   );
 }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
-import Input from "@material-ui/core/Input";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//Material UI Components
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import Button from "@material-ui/core/Button";
-import AppBar from "@material-ui/core/AppBar";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import TableContainer from "@material-ui/core/TableContainer";
 
 const usestyles = makeStyles((theme) => ({
   root: {
@@ -14,73 +29,178 @@ const usestyles = makeStyles((theme) => ({
       width: "25ch",
     },
   },
+  input: {
+    height: 35,
+    fontSize: 16,
+    borderRadius: 5,
+  },
 }));
-
-const initialFormData = Object.freeze({
-  firstName: "",
-  lastName: "",
-  phoneNumber: "",
-});
 
 export const MainPage = () => {
   const classes = usestyles();
+  const [names, setNames] = useState([]);
+  const [id, setId] = useState();
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+  });
+  const url = "http://localhost:8080/";
+  const [open, setOpen] = React.useState(false);
 
-  const [formData, updateFormData] = useState([initialFormData]);
-  const handleChange = (e) => {
-    updateFormData({
-      ...formData,
-
-      // Trimming any whitespace
-      [e.target.name]: e.target.value.trim(),
-    });
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-    // ... submit to API or something
-  }
+  const handleClose = () => {
+    setOpen(false);
+  };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(person);
+    let response = await axios.post(url, {
+      firstName: person.firstName,
+      lastName: person.lastName,
+      phoneNumber: person.phoneNumber,
+    });
+    console.log(response);
+    window.location.reload(true);
+  };
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    console.log(id);
+    let url = `http://localhost:8080/`;
+    await axios.delete(url, {
+      data: id,
+    });
+    handleClose();
+    window.location.reload(true);
+  };
+
+  useEffect(() => {
+    const getNames = async () => {
+      let response = await axios.get(url);
+      setNames(response.data);
+      //console.log('Names: ' + JSON.stringify(response.data))
+    };
+
+    getNames();
+  }, []);
 
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h3" class={classes.grow}>
+          <Typography variant="h3" className={classes.grow}>
             Names 'n Numbers
           </Typography>
         </Toolbar>
       </AppBar>
-      <form className={classes.root} type="submit">
-        <Grid container spacing={10}>
-          <Grid item xs={"auto"}>
-            <Input
-              name="firstName"
-              type="text"
-              placeholder="Enter First Name"
-              onChange={handleChange}
-            />
-            <Input
-              name="lastName"
-              type="text"
-              placeholder="Enter Last Name"
-              onChange={handleChange}
-            />
-            <Input
-              name="phoneNumber"
-              type="text"
-              placeholder="Enter Phone Number"
-              onChange={handleChange}
-            />
-            <br />
-            <br />
-            <Button variant="contained" onClick={handleSubmit}>
-              Submit
-            </Button>
-          </Grid>
-          
+      <form className={classes.root} type="submit" onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <input
+            className={classes.input}
+            id="firstName"
+            name="firstName"
+            type="text"
+            placeholder="&nbsp;&nbsp;Enter First Name"
+            onChange={(event) =>
+              setPerson({ ...person, firstName: event.target.value })
+            }
+          />
+          <input
+            className={classes.input}
+            id="lastName"
+            name="lastName"
+            type="text"
+            placeholder="&nbsp;&nbsp;Enter Last Name"
+            onChange={(event) =>
+              setPerson({ ...person, lastName: event.target.value })
+            }
+          />
+          <input
+            className={classes.input}
+            id="phoneNumber"
+            name="phoneNumber"
+            type="text"
+            placeholder="&nbsp;&nbsp;Enter Phone Number"
+            onChange={(event) =>
+              setPerson({ ...person, phoneNumber: event.target.value })
+            }
+          />
         </Grid>
+        <Button variant="contained" color={"primary"} onClick={handleSubmit}>
+          Add Person
+        </Button>
+        <Button variant="outlined" color="inherit" onClick={handleClickOpen}>
+          Delete Person
+        </Button>
       </form>
+      <br /> <br />
+      <Divider variant="middle" />
+      <Card>
+        <TableContainer>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">ID</TableCell>
+                <TableCell align="left">First Name</TableCell>
+                <TableCell align="left">Last Name</TableCell>
+                <TableCell align="left">Phone Number</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.entries(names).map(([id, name]) => (
+                <TableRow key={id}>
+                  <TableCell>{id}</TableCell>
+                  <TableCell>{name.firstName}</TableCell>
+                  <TableCell>{name.lastName}</TableCell>
+                  <TableCell>{name.phoneNumber}</TableCell>
+                  <TableCell align="left">
+                    <Button color={"primary"} variant={"contained"}>
+                      Edit
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Card>
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the ID of the person you would like to delete:
+            </DialogContentText>
+            <input
+              className={classes.input}
+              type="text"
+              placeholder="&nbsp;&nbsp;Enter ID Number"
+              onChange={(event) => setId(event.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleDelete} color="primary">
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      {/* <h2>First Name: {person.firstName}</h2>
+      <h2>Last Name: {person.lastName}</h2>
+      <h2>Phone Number: {person.phoneNumber}</h2> */}
     </>
   );
 };
